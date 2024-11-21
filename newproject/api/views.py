0b7bf51c7dece604f528678f8b5d295f9eb2663b
Mahadi <smmahadi01910048684@gmail.com>
2024-11-22 02:52:17 +0600

    making api i django

---
+++ newproject/api/views.py
@@ -0,0 +1,12 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializer import UserSerializer
+
+@api_view(['GET'])
+def get_user(request):
+    return Response(UserSerializer({'name': 'pedro', 'age': 23}))
+
+
+
